cb37ffb7d33212b724c51c91996aeb660c4d0f18
Phil Zona <pzona@launchdarkly.com>
2021-12-14 17:20:27 -0500

    Add some debugging stuff

--- dist/index.js
+++ dist/index.js
@@ -8442,6 +8442,7 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
+const fs = __nccwpck_require__(7147);
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
@@ -8452,6 +8453,14 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   core.setOutput('event', payload);
+
+  // Export the cwd for debugging
+  const cwd = process.cwd();
+  core.setOutput('cwd', cwd);
+
+  // Export file structure for debugging
+  const ls = fs.readdirSync(cwd);
+  core.setOutput('contents', ls);
 } catch(err) {
   core.setFailed(err.message);
 }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -8,6 +9,14 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   core.setOutput('event', payload);
+
+  // Export the cwd for debugging
+  const cwd = process.cwd();
+  core.setOutput('cwd', cwd);
+
+  // Export file structure for debugging
+  const ls = fs.readdirSync(cwd);
+  core.setOutput('contents', ls);
 } catch(err) {
   core.setFailed(err.message);
 }
