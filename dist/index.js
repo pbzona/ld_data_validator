66ca7b38c053ce5da5d731abb624b5934cf86d27
Yoz Grahame <ygrahame@launchdarkly.com>
2021-12-16 21:02:06 -0800

    it turns out you need to run the build step. Doh.

--- dist/index.js
+++ dist/index.js
@@ -11329,8 +11329,15 @@ const parseFlagKey = (pathToFile) => {
 }
 
 const parseFlagEnv = (pathToFile) => {
-  const flagConfigName = path.parse(pathToFile).name
-  return flagConfigName == 'core-metadata' ? 'global' : flagConfigName;
+  const flagConfigName = path.parse(pathToFile).name;
+  let flagEnv;
+  if (flagConfigName == 'core-metadata') {
+    flagEnv = 'global'; // early return
+  } else {
+    flagEnv = flagConfigName.replace("env-","");
+  }
+  console.log(`Env ${flagEnv} parsed from filename ${flagConfigName}`);
+  return flagEnv;
 }
 
 const parseFlagProject = (pathToFile) => {
@@ -11376,7 +11383,24 @@ const makeSyncRequest = async (project, env, flag, newConfig, oldConfig) => {
   }
   console.log(config);
   
-  const response = await axios(config);
+  const response = await axios(config).catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("Server responded with an error:");
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log("No response received for this request:");
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
   return response;
 }
 
@@ -11494,7 +11518,7 @@ exports.validate = (commits) => {
         console.warn('If you have somehow included human-generated commits in this push, revert them and push again');
         process.exit(0);
       } else {
-        console.log(`Validation passed for: ${commit.author.name}`);
+        console.log(`Validation passed for: ${commit.author.name} - yay!`);
       }
     }
 
