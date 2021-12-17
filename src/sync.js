1c5d20f4ed2c9131759d935f78caaeb08091c6eb
Yoz Grahame <173848+yozlet@users.noreply.github.com>
2021-12-16 17:32:52 -0800

    more error handling

--- src/sync.js
+++ src/sync.js
@@ -24,7 +24,24 @@ const makeSyncRequest = async (project, env, flag, newConfig, oldConfig) => {
   }
   console.log(config);
   
-  const response = await axios(config);
+  const response = await axios(config).catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log("Server responded with an error:");
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log("No response received for this request:");
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
   return response;
 }
 
